1a8d64a750e1abceff769824b6194a720a853ecd
kouki7315 <k.hamada@goalist.co.jp>
2018-05-01 18:13:30 +0900

    fix

--- src/jp/co/goalist/array/Question2.java
+++ src/jp/co/goalist/array/Question2.java
@@ -8,9 +8,11 @@ public class Question2 {
     public static void Q2() {
         int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         for (int i = 0; i < num.length; i++) {
-            // 二重のforを使いたかったんですが、改行が上手く扱えなくて力技でゴリ押してしまいました…
-            System.out.println(num[i] * 1 + " " + num[i] * 2 + " " + num[i] * 3 + " " + num[i] * 4 + " " + num[i] * 5
-                    + " " + num[i] * 6 + " " + num[i] * 7 + " " + num[i] * 8 + " " + num[i] * 9);
+            for (int j = 0; j < num.length; j++)
+            //ありがとう増田くん
+            System.out.print(num[i] * num[j] + " "); //これだけだと改行できなくて詰む
+            System.out.println(" "); //この文があることで改行が可能に。天才。
+            //戒めのため、最初に書いたゴリラコードも残します→(num[i] * 1 + " " + num[i] * 2 + " " + num[i] * 3 + " " + num[i] * 4 + " " + num[i] * 5 + " " + num[i] * 6 + " " + num[i] * 7 + " " + num[i] * 8 + " " + num[i] * 9);
         }
     }
 }
--- src/jp/co/goalist/hello/Hello.java
+++ src/jp/co/goalist/hello/Hello.java
@@ -2,8 +2,16 @@ package jp.co.goalist.hello;
 
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+   	
+	public static void main(String[] args) {
+		String[] name = {"コウキ", "コショー", "サトウ"};
+		String hello ="オッス！";
+		
+		
+        System.out.println(hello);
+        System.out.println("ぼくの名前は" + name[0] + "です。" + hello);
+        System.out.println("前には" + name[1] + "が座ってます");
+        System.out.println("隣には" + name[2] + "がいます");
     }
 
 }
