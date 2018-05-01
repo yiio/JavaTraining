b9a83e3d530438f6604f25bb5f61a635ca055f80
｛Tomoka Suzuki｝ <to.suzuki@goalist.co.jp>
2018-05-01 16:08:38 +0900

    add: FizzBuzz Q1

---
+++ src/jp/co/goalist/fizzbuzz/Question1.java
@@ -0,0 +1,20 @@
+package jp.co.goalist.fizzbuzz;
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        String aho = "アホか否か？";
+
+        for (int i = 1; i < 31; i++) {
+
+            if (i%3==0) {
+                aho = "アホ";
+            }else {
+                aho ="　　　";
+            }
+            System.out.println(i + "です。" + aho);
+        }
+
+        System.out.println(10%3);
+        }
+}
