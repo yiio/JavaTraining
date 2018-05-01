843210ecec62cbaed33c190568afd75f60418afe
tosuzuki1006 <to.suzuki@goalist.co.jp>
2018-05-01 18:53:57 +0900

    add:arrayQ2

---
+++ src/jp/co/goalist/array/Q2.java
@@ -0,0 +1,19 @@
+package jp.co.goalist.array;
+
+public class Q2 {
+    public static void main(String args[]) 
+    {
+          int kuku[][] = new int[9][9];
+          for( int i=0; i<9; i++ ) {
+            for( int j=0; j<9; j++ ){
+                kuku[i][j] = (i+1) * (j+1);
+            }
+          }
+          for( int i=0; i<9; i++ ) {
+            for( int j=0; j<9; j++ ) { 
+                System.out.print(kuku[i][j] + " "); 
+            }
+                System.out.println(); 
+          }
+    }
+}
