4d8de58edf48a5aceff035828cb6dfd87aca370b
mashio <m.iwasaki@goalist.co.jp>
2018-05-01 16:08:51 +0900

    add: FizzBuzz Q1

---
+++ src/jp/co/goalist/fizzbuzz/Question1.java
@@ -0,0 +1,9 @@
+package jp.co.goalist.fizzbuzz;
+
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println(9 % 3);
+
+    }
+}
