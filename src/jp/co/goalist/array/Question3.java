ff8d1dbee2857fdf4d4c69683a6cb6ac05ddce91
kouki7315 <k.hamada@goalist.co.jp>
2018-05-01 18:49:38 +0900

    add: array

---
+++ src/jp/co/goalist/array/Question3.java
@@ -0,0 +1,13 @@
+package jp.co.goalist.array;
+
+public class Question3 {
+    public static void main(String args[]) {
+        Q3();
+    }
+    public static void Q3() {
+        String[] newGraduates = {"岩崎", "古庄", "佐藤", "鈴木", "濱田", "増田亜", "増田秀"};
+        for(int i = 0; i < newGraduates.length; i++) {
+            System.out.println(newGraduates[i]);
+        }
+    }
+}
---
+++ src/jp/co/goalist/array/Question4.java
@@ -0,0 +1,19 @@
+package jp.co.goalist.array;
+
+public class Question4 {
+    public static void main(String args[]) {
+        Q4();
+    }
+    public static void Q4() {
+        String[][] goalistAll = {
+                {"岩崎", "古庄", "佐藤", "鈴木", "濱田", "増田亜", "増田秀"},
+                {"入倉", "盛次", "飯尾", "チナパ"},
+                {"三井", "清水", "長田"}
+            };
+        for(int i = 0; i < goalistAll.length; i++) {
+            for(int j = 0; j < goalistAll[i].length; j++) {
+                System.out.println(goalistAll[i][j]);
+            }
+        }
+    }
+}
