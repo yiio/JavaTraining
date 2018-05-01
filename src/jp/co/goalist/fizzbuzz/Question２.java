011ac4c05ca60e333a3207d4fc4af540b7cc29bb
Satococo <y.sato@goalist.co.jp>
2018-05-01 16:57:04 +0900

    add:FizzBuzz Q2

---
+++ src/jp/co/goalist/fizzbuzz/Question２.java
@@ -0,0 +1,18 @@
+package jp.co.goalist.fizzbuzz;
+
+public class Question２ {
+    public static void main(String[] args) {
+    for (int num = 1; num <= 30; num++) {
+        String numStr = String.valueOf(num);
+        if (numStr.contains("3")) {
+            System.out.println(num + "です。アホ");
+
+        }else if(num % 3 == 0) {
+            System.out.println(num + "です。アホ");
+        }else {
+            System.out.println(num + "です。");
+        }
+    }
+
+}
+}
