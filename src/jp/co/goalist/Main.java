17bd5f9bb55d3329d9262464d91e927982c3d2b4
HITYORI <m.iwasaki@goalist.co.jp>
2018-05-02 15:20:11 +0900

    文字列操作１～４

--- src/jp/co/goalist/Main.java
+++ src/jp/co/goalist/Main.java
@@ -3,12 +3,43 @@ package jp.co.goalist;
 public class Main {
     public static void main(String[] args) {
         String str = "個と組織をGOALへ導くプロフェッショナルチーム";
+
+
         printStrLength(str);
+        goal(str);
+        ko(str);
+        sosiki(str);
+
     }
 
     public static void printStrLength(String str) {
-        
+
         System.out.println("渡された文字は"+str.charAt(2)+"文字です");
 
     }
+    public static void goal(String str) {
+        System.out.println("渡された文字は"+str.substring(2,6)+"文字です");
+    }
+    public static void ko(String str) {
+        int size =str.length();
+        int cut_length=1;
+        System.out.println("渡された文字は"+str.charAt(0)+str.substring(size-cut_length)+"文字です");
+    }
+    public static void sosiki(String str) {
+        int right=str.length();
+        int cut =3;
+        System.out.println("渡された文字は"+str.substring(right-cut)+"文字です");
+    }
+    
+    
+
+
+
+
+
+
+
+
 }
+
+
