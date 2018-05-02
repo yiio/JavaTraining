fd73dd910b8e98a67219e72e6f76330ef59a22a7
tosuzuki1006 <to.suzuki@goalist.co.jp>
2018-05-02 14:09:31 +0900

    arrayQ7

---
+++ src/jp/co/goalist/array/Q7.java
@@ -0,0 +1,34 @@
+package jp.co.goalist.array;
+
+public class Q7 {
+    public static void main(String[] args) {
+
+        // 名前とチームの配列
+        final String[][] teamArys = { { "山田", "Aチーム" }, { "田中", "Aチーム" }, { "鈴木", "Bチーム" }, { "中村", "Bチーム" },
+                { "伊藤", "Bチーム" } };
+
+        // 名前と得点の配列
+        final String[][] pointArys = { { "山田", "1" }, { "山田", "3" }, { "山田", "1" }, { "伊藤", "4" }, { "山田", "2" },
+                { "中村", "1" }, { "中村", "1" }, { "山田", "3" }, { "鈴木", "2" }, { "山田", "4" }, { "伊藤", "5" }, { "山田", "2" },
+                { "鈴木", "3" }, { "山田", "1" }, { "田中", "2" } };
+
+        int pointA = 0;
+        int pointB = 0;
+
+        for (int i = 0; i < pointArys.length; i++) {
+            if (pointArys[i][0] == "山田" || pointArys[i][0] == "田中") {
+                pointA += Integer.parseInt(pointArys[i][1]);
+            } else {
+
+                pointB += Integer.parseInt(pointArys[i][1]);
+            }
+        }
+
+        // 点数を出す
+        System.out.println("Aチームの合計得点は、" + pointA + "点です。");
+        System.out.println("Bチームの合計得点は、" + pointB + "点です。");
+
+    }
+
+}
+
