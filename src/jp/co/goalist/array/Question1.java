e419f961d13e54f85b3eed3a4a5d35a0df6b866d
arisamasuda <a.masuda@goalist.co.jp>
2018-05-02 11:13:40 +0900

    配列、課題1-2修正

--- src/jp/co/goalist/array/Question1.java
+++ src/jp/co/goalist/array/Question1.java
@@ -2,6 +2,8 @@ package jp.co.goalist.array;
 
 public class Question1 {
     public static void main(String[] args) {
+
+        // 課題1
         int[] ary1 = { 1, 2, 3, 4, 5 };
         int[] ary2 = { 10, 20, 30, 40, 50 };
 
@@ -12,6 +14,7 @@ public class Question1 {
 
         }
 
+        // 課題2
         int[] ary3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         for (int k = 0; k < ary3.length; k++) {
@@ -23,7 +26,25 @@ public class Question1 {
             }
 
         }
+        
+        
+        caluculation();
 
+    }
+
+    public static void caluculation() {
+        int[] numAry = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        String number = "";
+        
+        for (int i = 0; i < numAry.length; i ++) {
+            for (int j = 0; j < numAry.length; j ++) {
+                number += String.valueOf(numAry[i] * numAry[j]);
+                
+            }
+            System.out.println(number);
+            
+            number = "";
+        }
     }
 
 }
