56829e0edda5ac9047848a958239d960955e4d39
Satococo <y.sato@goalist.co.jp>
2018-05-02 11:53:35 +0900

    改行直しました

--- src/jp/co/goalist/array/Question2.java
+++ src/jp/co/goalist/array/Question2.java
@@ -10,8 +10,10 @@ public class Question2 {
         int[] numbers2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         for (int i = 0; i < numbers1.length; i++) {
             for (int j = 0; j < numbers2.length; j++) {
-                System.out.println(numbers1[i] * numbers2[j]);
+                System.out.print(numbers1[i] * numbers2[j] + " ");
+
             }
+            System.out.println(" ");
         }
 
     }
