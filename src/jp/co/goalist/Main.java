5f1ebd0b9000dbfc69b7077a12b1c6e4ceaed352
HITYORI <m.iwasaki@goalist.co.jp>
2018-05-02 14:40:27 +0900

    文字列　課題１

---
+++ src/jp/co/goalist/Main.java
@@ -0,0 +1,14 @@
+package jp.co.goalist;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "個と組織をGOALへ導くプロフェッショナルチーム";
+        printStrLength(str);
+    }
+
+    public static void printStrLength(String str) {
+        
+        System.out.println("渡された文字は"+str.charAt(2)+"文字です");
+
+    }
+}
