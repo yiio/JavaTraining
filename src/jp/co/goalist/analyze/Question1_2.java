a46cb49541fa3ebd408aba67455986a5190bccab
arisamasuda <a.masuda@goalist.co.jp>
2018-05-02 17:15:55 +0900

    ファイル読み込み・集計、課題1_マップ使用

--- src/jp/co/goalist/analyze/Question1_2.java
+++ src/jp/co/goalist/analyze/Question1_2.java
@@ -17,37 +17,26 @@ public class Question1_2 {
             String line;
 
             // マップの作製
-            Map<String, Integer> prefecture = new HashMap<String, Integer>() {
-                {
-                    put("東京都", 0);
-                    put("神奈川県", 0);
-                    put("兵庫県", 0);
-                    put("大阪府", 0);
-                    put("京都府", 0);
-                    put("千葉県", 0);
-                    put("埼玉県", 0);
-                    put("奈良県", 0);
 
-                }
-            };
+            Map<String, Integer> prefecture = new HashMap<String, Integer>();
 
-            
             // 件数のカウント
             while ((line = br.readLine()) != null) {
-                int i = prefecture.get(line);
+                int i = 0;
+                if (!prefecture.containsKey(line)) {
+                    prefecture.put(line, i);
+                }
+                i = prefecture.get(line);
                 i++;
-                prefecture.replace(line, i);
+                prefecture.put(line, i);
             }
 
-            
             // 件数の書き出し
             for (String key : prefecture.keySet()) {
                 System.out.println(key + "は、" + prefecture.get(key) + "件です。");
             }
 
-        } catch (
-
-        IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
