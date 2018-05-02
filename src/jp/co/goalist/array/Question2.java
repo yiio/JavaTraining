4830b87a43eed9e9710624e3622381f20b40b5c2
F-kosho <f.kosho@goalist.co.jp>
2018-05-02 12:56:41 +0900

    配列２を修正しました

--- src/jp/co/goalist/array/Question2.java
+++ src/jp/co/goalist/array/Question2.java
@@ -1,21 +1,13 @@
 package jp.co.goalist.array;
 
 public class Question2 {
-
     public static void main(String[] args) {
-        int kuku[][] = new int[9][9];
-        
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                kuku[i][j] = (i + 1) * (j + 1);
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i * j + " ");
             }
-        }
-        
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(kuku[i][j] + " ");
-            }
-                System.out.println();
+            System.out.println();
         }
     }
+
 }
