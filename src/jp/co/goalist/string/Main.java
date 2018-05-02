e0d1fa067edf8f618a556f7cff99a5938bbec240
Satococo <y.sato@goalist.co.jp>
2018-05-02 09:56:24 +0900

    add: String Q1~Q4

---
+++ src/jp/co/goalist/string/Main.java
@@ -0,0 +1,31 @@
+package jp.co.goalist.string;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        String str = "個と組織をGOALへ導くプロフェッショナルチーム";
+
+        printStrLength(str);
+             Question1(str);
+             Question2(str);
+             Question3(str);
+             Question4(str);
+ }
+       private static void printStrLength(String str){
+         System.out.println("渡された文字は " + str.length() + " 文字です");
+
+       }
+       private static void Question1(String str){
+         System.out.println(str.charAt(2));
+       }
+       private static void Question2(String str){
+         System.out.println(str.substring(2,5));
+       }
+       private static void Question3(String str){
+         System.out.println(str.charAt(0) + str.substring(str.length()-1));
+       }
+       private static void Question4(String str){
+         System.out.println(str.substring(str.length() - 3));
+       }
+}
