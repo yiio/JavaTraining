e924030bfd2f063b8bcf4849e634662d40d8ce0e
Satococo <y.sato@goalist.co.jp>
2018-05-02 10:44:06 +0900

    add:array Q1,Q2

---
+++ src/jp/co/goalist/array/Question2.java
@@ -0,0 +1,18 @@
+package jp.co.goalist.array;
+
+public class Question2 {
+    public static void main(String[] args) {
+        multiplication();
+    }
+
+    public static void multiplication() {
+        int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] numbers2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int i = 0; i < numbers1.length; i++) {
+            for (int j = 0; j < numbers2.length; j++) {
+                System.out.println(numbers1[i] * numbers2[j]);
+            }
+        }
+
+    }
+}
