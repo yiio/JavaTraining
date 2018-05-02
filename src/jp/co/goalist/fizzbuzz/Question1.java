caa456363ebe7fe95653efb3090f433eccdbb3bf
HITYORI <m.iwasaki@goalist.co.jp>
2018-05-02 10:12:40 +0900

    課題1です

--- src/jp/co/goalist/fizzbuzz/Question1.java
+++ src/jp/co/goalist/fizzbuzz/Question1.java
@@ -2,8 +2,17 @@ package jp.co.goalist.fizzbuzz;
 
 public class Question1 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.println(9 % 3);
+
+        
+       for(int i=1;i<=30;i++) {
+           if(i%3 ==0) {
+               System.out.println(i+"です。アホ");
+           }else {
+               System.out.println(i+"です");
+           }
+       }
+        
+        
 
     }
 }
