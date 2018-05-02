b942a61108bf0118741cd8f9d64369bddb93e843
tosuzuki1006 <to.suzuki@goalist.co.jp>
2018-05-02 11:55:35 +0900

    arrayQ5

---
+++ src/jp/co/goalist/array/Q5.java
@@ -0,0 +1,15 @@
+package jp.co.goalist.array;
+
+public class Q5 {
+    public static void main(String[] args) {
+        String[][] words = { { "さるが", "とりが", "いぬが" }, { "山で", "川で", "鬼ヶ島で" }, { "洗濯をした", "芝刈りをした", "鬼退治をした" } };
+        for (int i = 0; i < words[0].length; i++) {
+            for (int j = 0; j < words[1].length; j++) {
+                for (int k = 0; k < words[2].length; k++) {
+
+                    System.out.println(words[0][i] + words[1][j] + words[2][k]);
+                }
+            }
+        }
+    }
+}
