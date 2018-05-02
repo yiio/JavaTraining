f08c9d52762f2f91b5778d5c8d67e9757bf4e86a
HITYORI <m.iwasaki@goalist.co.jp>
2018-05-02 11:19:14 +0900

    課題２です

---
+++ src/jp/co/goalist/fizzbuzz/Question2.java
@@ -0,0 +1,21 @@
+package jp.co.goalist.fizzbuzz;
+
+public class Question2 {
+    public static void main(String[] args) {
+        for(int num=1;num<=30;num++) {
+            String numStr = String.valueOf(num);
+
+            if(numStr.contains("3")){
+                System.out.println(num+"です。アホ");
+            }else if(num%3==0) {
+                System.out.println(num+"です。アホ");
+
+                }else {
+                    System.out.println(num+"です。");
+            }
+
+        }
+
+    }
+
+}
