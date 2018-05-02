be1518f189d388f115ade2996be9cc138947bed6
tosuzuki1006 <to.suzuki@goalist.co.jp>
2018-05-02 14:07:28 +0900

    インデント直しました

--- src/jp/co/goalist/array/Q3.java
+++ src/jp/co/goalist/array/Q3.java
@@ -1,13 +1,13 @@
 package jp.co.goalist.array;
 
 public class Q3 {
-  public static void main(String[]args) {
-      String[] newGraduates = {"岩崎", "古庄", "佐藤", "鈴木", "濱田", "増田1", "増田2"};
-      
-      for(String newGraduate:newGraduates){
-          System.out.println(newGraduate);
-          }
-        
+    public static void main(String[] args) {
+        String[] newGraduates = { "岩崎", "古庄", "佐藤", "鈴木", "濱田", "増田1", "増田2" };
+
+        for (String newGraduate : newGraduates) {
+            System.out.println(newGraduate);
+        }
+
     }
 
 }
