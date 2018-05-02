903110a00f2d1c347e34d7ad5079ef17e1f883e8
tosuzuki1006 <to.suzuki@goalist.co.jp>
2018-05-02 15:08:18 +0900

    修正しました

--- src/jp/co/goalist/array/Q8.java
+++ src/jp/co/goalist/array/Q8.java
@@ -12,30 +12,28 @@ public class Q8 {
                 { "中村", "1" }, { "中村", "1" }, { "山田", "3" }, { "鈴木", "2" }, { "山田", "4" }, { "伊藤", "5" }, { "山田", "2" },
                 { "鈴木", "3" }, { "山田", "1" }, { "田中", "2" } };
 
-        int pointA1 = 0;
-        int pointB1 = 0;
+        int pointA = 0;
+        int pointB = 0;
         int pointC = 0;
         int pointD = 0;
-        {
 
-            for (int i = 0; i < pointArys.length; i++) {
-                if (pointArys[i][0] == "山田" || pointArys[i][0] == "田中") {
-                    pointA1 += Integer.parseInt(pointArys[i][1]);
-                } else if (pointArys[i][0] == "鈴木") {
-                    pointB1 += Integer.parseInt(pointArys[i][1]);
-                } else if (pointArys[i][0] == "中村") {
-                    pointC += Integer.parseInt(pointArys[i][1]);
-                } else {
-                    pointD += Integer.parseInt(pointArys[i][1]);
-                }
+        for (int i = 0; i < pointArys.length; i++) {
+            if (pointArys[i][0] == "山田" || pointArys[i][0] == "田中") {
+                pointA += Integer.parseInt(pointArys[i][1]);
+            } else if (pointArys[i][0] == "鈴木") {
+                pointB += Integer.parseInt(pointArys[i][1]);
+            } else if (pointArys[i][0] == "中村") {
+                pointC += Integer.parseInt(pointArys[i][1]);
+            } else {
+                pointD += Integer.parseInt(pointArys[i][1]);
             }
-
-            // 点数を出す
-            System.out.println("Aチームの合計得点は、" + pointA1 + "点です。");
-            System.out.println("Bチームの合計得点は、" + pointB1 + "点です。");
-            System.out.println("Cチームの合計得点は、" + pointC + "点です。");
-            System.out.println("Dチームの合計得点は、" + pointD + "点です。");
-
         }
+
+        // 点数を出す
+        System.out.println("Aチームの合計得点は、" + pointA + "点です。");
+        System.out.println("Bチームの合計得点は、" + pointB + "点です。");
+        System.out.println("Cチームの合計得点は、" + pointC + "点です。");
+        System.out.println("Dチームの合計得点は、" + pointD + "点です。");
+
     }
 }
