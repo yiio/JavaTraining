6c0e33a562c306be1297bf5cd56d54ef1591a6ea
kanaria0822 <s.masuda@goalist.co.jp>
2018-05-03 12:54:18 +0900

    modify little

--- src/jp/co/goalist/string/Main.java
+++ src/jp/co/goalist/string/Main.java
@@ -5,24 +5,17 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        String str;
-        /*
-        try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("input : ");
-            str = br.readLine();
-            
-        }catch(IOException e){
-            System.out.println(e);
-        }
-        */
-        str = "abcdefghai";
+        String str = "abcdefghai";
+        
         System.out.print("c ? ");
         show3rd(str);
+        
         System.out.print("cdef ? ");
         showFrom3rdTo6th(str);
+        
         System.out.print("ai ? ");
         show1stAndLast(str);
+        
         System.out.print("h ? ");
         showBehind3rd(str);
     }
@@ -36,7 +29,7 @@ public class Main {
     }
     
     public static void show1stAndLast(String str){
-        System.out.println(str.charAt(0)+""+str.charAt(str.length()-1));
+        System.out.println(str.charAt(0) + str.charAt(str.length()-1));
     }
     
     public static void showBehind3rd(String str){
