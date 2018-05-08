c3e61eb4f99470f748a5de300775df4579ae0afa
F-kosho <f.kosho@goalist.co.jp>
2018-05-08 12:58:28 +0900

    analyzeの課題２を追加しました

--- src/jp/co/goalist/fizzbuzz/Question2.java
+++ src/jp/co/goalist/fizzbuzz/Question2.java
@@ -4,9 +4,10 @@ public class Question2 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 30; i++) {
+            String iStr = String.valueOf(i);
             if (i % 3 == 0) {
                 System.out.println(i + "です。アホ");
-            } else if (i.contains("3")) {
+            } else if (iStr.contains("3")) {
                 System.out.println(i + "です。アホ");
             } else {
                 System.out.println(i + "です。");
