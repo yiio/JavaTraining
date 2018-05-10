e7933a6612cfb92c5ff7eceb2a989ac9c3528b31
F-kosho <f.kosho@goalist.co.jp>
2018-05-10 17:24:26 +0900

    analyzeの課題３が完成しました

--- src/jp/co/goalist/analyze/Question3.java
+++ src/jp/co/goalist/analyze/Question3.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Question3 {
@@ -17,7 +18,7 @@ public class Question3 {
             String line;
             int cnt = 0;
             String[] subjectAry = {};
-            Map<String, Integer> subjectScoreMap = new HashMap<String, Integer>(); // 科目名, 最高得点
+            Map<String, Integer> subjectScoreMap = new LinkedHashMap<String, Integer>(); // 科目名, 最高得点
             Map<String, String> subjectNameMap = new HashMap<String, String>(); // 科目名, 最高得点者名
             
             while ((line = br.readLine()) != null) {
@@ -31,18 +32,26 @@ public class Question3 {
                     }
                     continue;
                 }
+                // 2行目以降
                 for (int i = 1; i < cols.length; i++) { // その行を科目ごとに処理
                     int max = subjectScoreMap.get(subjectAry[i]);
                     int score = Integer.valueOf(cols[i]);
                     if (max < score) {
+                        // subjectAry[i]は科目名、cols[0]は最高得点者名
                         subjectScoreMap.put(subjectAry[i], score); // その時点での最高点として、科目：最高点をマップに格納
                         subjectNameMap.put(subjectAry[i], cols[0]); // その時点での最高得点者として、科目：最高得点者をマップに格納
                     }
                 }
                 
             }
-            System.out.println(subjectScoreMap);
-            System.out.println(subjectNameMap);
+            // 結果を出力
+            for (Map.Entry<String, Integer> entry : subjectScoreMap.entrySet()) {
+                String subject = entry.getKey(); // 科目名
+                String name = subjectNameMap.get(entry.getKey()); // 最高得点者名
+                int maxScore = entry.getValue();
+                System.out.println(subject + "の最高得点者は、" + name + "さん、" + maxScore + "点です。");
+            }
+            
         }catch (IOException e) {
             e.printStackTrace();
         }
