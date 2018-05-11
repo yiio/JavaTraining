999b090da84624d08d854d8dffeb678fc8fec079
Satococo <y.sato@goalist.co.jp>
2018-05-11 11:37:57 +0900

    TreeMapを降順にソート

--- src/jp/co/goalist/analyze/Question6.java
+++ src/jp/co/goalist/analyze/Question6.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,7 +36,13 @@ public class Question6 {
 
         int[] sums = new int[lineCount1];
         int lineCount2 = 0;
-        Map<Integer, String> sumMap = new TreeMap<>();
+
+        // <合計点数,名前>のTreeMapを作成、降順にソート
+        Map<Integer, String> sumMap = new TreeMap<>(new Comparator<Integer>() {
+            public int compare(Integer m, Integer n) {
+                return ((Integer) m).compareTo(n) * -1;
+            }
+        });
 
         try (BufferedReader br2 = Files.newBufferedReader(filePath)) {
 
@@ -57,25 +64,21 @@ public class Question6 {
                     sums[lineCount2] += Integer.parseInt(pointList1.get(x));
                 }
 
-                sumMap.put(sums[lineCount2], point[0]); // <合計点数,名前>
+                sumMap.put(sums[lineCount2], point[0]);
                 lineCount2++;
 
             }
 
-            // TreeMapによってValueで昇順にソートされた点数をリストへ追加
+            // TreeMapによってValueで降順にソートされた点数をリストへ追加
             List<Integer> pointList2 = new ArrayList<>(sumMap.keySet());
 
-            // (人数)-(pointList内の要素番号)で順位を決定
+            // 順位を決定、出力
             for (int x = 0; x < lineCount2; x++) {
+                int rank = x + 1;
                 int result = pointList2.get(x);
-                int rank = lineCount2 - x;
                 rankMap.put(rank, result);
                 nameMap.put(rank, sumMap.get(result));
-            }
-
-            // 結果を出力
-            for (int x = 1; x <= lineCount2; x++) {
-                System.out.println(x + "位: " + nameMap.get(x) + "さん　" + rankMap.get(x) + "点");
+                System.out.println(rank + "位: " + nameMap.get(rank) + "さん　" + rankMap.get(rank) + "点");
             }
 
         } catch (IOException e) {
