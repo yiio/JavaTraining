1a13eb21434f6507b96e0cd22c9d4c1099e64069
yiio <y.iio@goalist.co.jp>
2018-05-14 18:12:29 +0900

    update: analyze Q3

--- src/jp/co/goalist/analyze/Question3.java
+++ src/jp/co/goalist/analyze/Question3.java
@@ -14,27 +14,20 @@ public class Question3 {
         Path filePath = Paths.get("/Users/yiio/workspace/resources/testResult.csv");
         try (BufferedReader br = Files.newBufferedReader(filePath)) {
 
-            String[] header = {}; // ヘッダー行を配列として格納するための変数
             Map<String, Integer> subjectPointMap = new HashMap<String, Integer>(); // 科目名と最高点の対応表
             Map<String, String> subjectNameMap = new HashMap<String, String>(); // 科目名と最高得点者の対応表
-
-            // CSVを1行ずつまわしていく
             String line; // 1行分の文字列を格納する変数
-            int cnt = 0; // 何行目
-            while ((line = br.readLine()) != null) {
-                cnt++;
+
+            // まずはヘッダ行の処理
+            line = br.readLine(); // 1行目を読み込む
+            String[] header = line.split(","); // ヘッダー行をカンマ区切りで配列に分割し、配列として格納する
+            for (int i = 1; i < header.length; i++) {
+                subjectPointMap.put(header[i], 0); // 科目名と最高点の対応表を初期化しておく
+            }
+
+            // ヘッダ行以外の処理
+            while ((line = br.readLine()) != null) { // whileループしつつ2行目以降を読み込む
                 String[] cols = line.split(","); // 1行をカンマ区切りで配列に分割する
-
-                // ヘッダ行の処理
-                if (cnt == 1) {
-                    header = cols; // ヘッダー行に格納
-                    for (int i = 1; i < cols.length; i++) {
-                        subjectPointMap.put(cols[i], 0); // 科目名と最高点の対応表を初期化しておく
-                    }
-                    continue; // whileループの最初に戻る
-                }
-
-                // ヘッダ行以外の処理
                 String name = cols[0]; // 名前は0番目
                 for (int i = 1; i < cols.length; i++) { // 科目ごとに最高点を比較していく
                     String subject = header[i]; // この科目名
