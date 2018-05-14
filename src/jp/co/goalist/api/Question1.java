c6600f82f76984785b084db50fb344c6d54e7a22
arisamasuda <a.masuda@goalist.co.jp>
2018-05-14 16:14:50 +0900

    WebAPI、課題1

---
+++ src/jp/co/goalist/api/Question1.java
@@ -0,0 +1,60 @@
+package jp.co.goalist.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Question1 {
+    public static void main(String[] args) {
+        String message = "Hello!"; // 出力するメッセージ
+        String strUrl = "https://api.chatwork.com/v2/rooms/105172471/messages";
+        String api = "ひみつ";
+
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(strUrl);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoOutput(true); // URL接続を出力用として使用, POSTを可能にする
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("X-ChatWorkToken", api); // ヘッダを設定
+            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream()); // POST用のOutputStreamを取得
+
+            String messageEnc = URLEncoder.encode(message, "UTF-8");
+            out.write("body=" + messageEnc); // データをPOSTする
+            out.close();
+
+            connection.connect();
+
+            InputStream in = connection.getInputStream();
+            InputStreamReader inReader = new InputStreamReader(in, "UTF-8");
+            BufferedReader bufReader = new BufferedReader(inReader);
+
+            String line = null;
+            while ((line = bufReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (ProtocolException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+    }
+
+}
