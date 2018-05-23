580062a1fbdf8eddb9546ce1a10356cc081ec1cf
kouki7315 <k.hamada@goalist.co.jp>
2018-05-23 12:46:46 +0900

    modify crawler : Q1

--- src/jp/co/goalist/crawler/Question1.java
+++ src/jp/co/goalist/crawler/Question1.java
@@ -7,138 +7,33 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public  class Question1 {
-    public static void main(String[] args)  throws IOException {
-        Overall(args);
-        Business(args);
-        Digital(args);
-        Kurashi(args);
-        Entertainment(args);
-
-    }
-
-    // 総合カテゴリ
-    public static void Overall(String[] args) throws IOException {
-        System.out.println("■総合\n");
-        String rootUrl = "http://news.mynavi.jp/top/headline";
+    public static void main(String[] args) throws IOException {
+        String rootUrl = "https://news.mynavi.jp/";
         Document doc = Jsoup.connect(rootUrl).get();
-        Element el = doc.select("div.thumb-s").get(0);
-        //System.out.println(el.toString());
-
-        for (Element child : el.children()) {
-            if (child.children().size() < 1) {
+        Element tab = doc.select("ul.tab-nav").get(0);
+        for (Element child1 : tab.children()) {
+            if (child1.children().size() < 1) {
                 continue;
             }
+            String category = child1.text();
+            System.out.println("■" + category);
+            String tag = child1.select("a").attr("href");
+            Element el = doc.select(tag + "> div.tab__list > ul").get(0);
+            for (Element child2 : el.children()) {
+                if (child2.children().size() < 1) {
+                    continue;
+                }
+                //ニュースのタイトル
+                String title = child2.text();
+                //ニュースのURL
+                String url = child2.child(0).attr("href");
 
-            //ニュースのタイトル
-            String title = child.select("a").get(0).text();
-
-            //ニュースのURL
-            String url = child.child(0).attr("href");
-            url = rootUrl + url;
-
-            System.out.println(title + "\n(" + url + ")\n");
-            //System.out.println(doc.html());
+                System.out.println(title + "\n(" + url + ")\n");
+            }
         }
     }
 
 
-    // ビジネスカテゴリ
-    public static void Business(String[] args) throws IOException {
-        System.out.println("\n■ビジネス\n");
-        String rootUrl = "https://news.mynavi.jp/list/headline/business/";
-        Document doc = Jsoup.connect(rootUrl).get();
-        Element el = doc.select("div.thumb-s").get(0);
-        //System.out.println(el.toString());
 
-        for (Element child : el.children()) {
-            if (child.children().size() < 1) {
-                continue;
-            }
 
-            //ニュースのタイトル
-            String title = child.select("a").get(0).text();
-
-            //ニュースのURL
-            String url = child.child(0).attr("href");
-            url = rootUrl + url;
-
-            System.out.println(title + "\n(" + url + ")\n");
-            //System.out.println(doc.html());
-        }
-    }
-
-    // デジタルカテゴリ
-    public static void Digital(String[] args) throws IOException {
-        System.out.println("\n■デジタル\n");
-        String rootUrl = "https://news.mynavi.jp/list/headline/digital/";
-        Document doc = Jsoup.connect(rootUrl).get();
-        Element el = doc.select("div.thumb-s").get(0);
-        //System.out.println(el.toString());
-
-        for (Element child : el.children()) {
-            if (child.children().size() < 1) {
-                continue;
-            }
-
-            //ニュースのタイトル
-            String title = child.select("a").get(0).text();
-
-            //ニュースのURL
-            String url = child.child(0).attr("href");
-            url = rootUrl + url;
-
-            System.out.println(title + "\n(" + url + ")\n");
-            //System.out.println(doc.html());
-        }
-    }
-
-    // ライフカテゴリ
-    public static void Kurashi(String[] args) throws IOException {
-        System.out.println("\n■ライフ\n");
-        String rootUrl = "https://news.mynavi.jp/list/headline/kurashi/";
-        Document doc = Jsoup.connect(rootUrl).get();
-        Element el = doc.select("div.thumb-s").get(0);
-        //System.out.println(el.toString());
-
-        for (Element child : el.children()) {
-            if (child.children().size() < 1) {
-                continue;
-            }
-
-            //ニュースのタイトル
-            String title = child.select("a").get(0).text();
-
-            //ニュースのURL
-            String url = child.child(0).attr("href");
-            url = rootUrl + url;
-
-            System.out.println(title + "\n(" + url + ")\n");
-            //System.out.println(doc.html());
-        }
-    }
-
-    // エンタメカテゴリ
-    public static void Entertainment(String[] args) throws IOException {
-        System.out.println("\n■エンタメ\n");
-        String rootUrl = "https://news.mynavi.jp/list/headline/entertainment/";
-        Document doc = Jsoup.connect(rootUrl).get();
-        Element el = doc.select("div.thumb-s").get(0);
-        //System.out.println(el.toString());
-
-        for (Element child : el.children()) {
-            if (child.children().size() < 1) {
-                continue;
-            }
-
-            //ニュースのタイトル
-            String title = child.select("a").get(0).text();
-
-            //ニュースのURL
-            String url = child.child(0).attr("href");
-            url = rootUrl + url;
-
-            System.out.println(title + "\n(" + url + ")\n");
-            //System.out.println(doc.html());
-        }
-    }
 }
