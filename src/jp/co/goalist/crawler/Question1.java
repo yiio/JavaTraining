04dc61b72eef5317caf50155581531f5196343e8
kouki7315 <k.hamada@goalist.co.jp>
2018-05-23 13:21:46 +0900

    modify crawler : Q1

--- src/jp/co/goalist/crawler/Question1.java
+++ src/jp/co/goalist/crawler/Question1.java
@@ -11,29 +11,26 @@ public  class Question1 {
         String rootUrl = "https://news.mynavi.jp/";
         Document doc = Jsoup.connect(rootUrl).get();
         Element tab = doc.select("ul.tab-nav").get(0);
-        for (Element child1 : tab.children()) {
+        for (Element child1 : tab.children()) { // カテゴリー毎に回していく
             if (child1.children().size() < 1) {
                 continue;
             }
             String category = child1.text();
             System.out.println("■" + category);
-            String tag = child1.select("a").attr("href");
-            Element el = doc.select(tag + "> div.tab__list > ul").get(0);
+
+            String tag = child1.select("a").attr("href"); // タグを拾ってくる
+            Element el = doc.select(tag + "> div.tab__list > ul").get(0); // 記事を取り出すため要素を切り取る
+
             for (Element child2 : el.children()) {
                 if (child2.children().size() < 1) {
                     continue;
                 }
-                //ニュースのタイトル
-                String title = child2.text();
-                //ニュースのURL
-                String url = child2.child(0).attr("href");
+
+                String title = child2.text(); // ニュースのタイトル
+                String url = child2.child(0).attr("href"); // ニュースのURL
 
                 System.out.println(title + "\n(" + url + ")\n");
             }
         }
     }
-
-
-
-
 }
