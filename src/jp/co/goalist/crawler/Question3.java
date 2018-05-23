54e5a10148ff61f6b4cabdf0735728b153b1e11b
kouki7315 <k.hamada@goalist.co.jp>
2018-05-23 12:05:21 +0900

    add crawler : Q3

---
+++ src/jp/co/goalist/crawler/Question3.java
@@ -0,0 +1,82 @@
+package jp.co.goalist.crawler;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class Question3 {
+    public static void main(String[] args) {
+        System.out.println("クロール＆スクレイプ処理 を開始しました。");
+
+        makeArticlesCsv(2016, 12); // 2016年12月の記事の一覧
+
+        System.out.println("クロール＆スクレイプ処理 を終了しました。");
+    }
+
+    private static void makeArticlesCsv(int year, int month) {
+        Path answer = Paths.get("C:/TechTraining/resources/answer.csv");
+        try (BufferedWriter bw = Files.newBufferedWriter(answer)) {
+            bw.write("件名,カテゴリ,ニュース日時,URL");
+            bw.newLine();
+
+            String rootUrl = "http://news.mynavi.jp";
+            String subUrl = rootUrl + "/list/headline/" + year + "/" + month; // 指定した年月のページ
+
+            int i = 1;
+
+            while(i < 300) {
+                try {
+                    String headlineUrl = subUrl  + "/?page=" + i; // 何ページ目かを指定
+                    Document doc = Jsoup.connect(headlineUrl).get();
+                    Element el = doc.select("div.thumb-s").get(0);
+
+                    for (Element child : el.children()) {
+                        if (child.children().size() < 1) {
+                            continue;
+                        }
+
+                        //ニュースのタイトル
+                        String title = child.select("a").get(0).text();
+
+                        //カテゴリ
+                        String category = null;
+                        try {
+                            category = child.select("a").get(1).text();
+                        } catch (IndexOutOfBoundsException e) {
+                            category = " ";
+                        }
+
+                        // 日時
+                        String date =year + "/"+child.select("span.thumb-s__date").get(0).text();
+
+                        //ニュースのURL
+                        String url = child.select("a").get(0).attr("href");
+                        url = rootUrl + url;
+
+                        String str = title + ","+ category + "," + date + "," + url  ;
+                        System.out.println(str);
+                        bw.write(str);
+                        bw.newLine();
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    i++;
+                } catch(IndexOutOfBoundsException e) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
