3867a655f72c033294adee0742ad51e3f3ece91a
kouki7315 <k.hamada@goalist.co.jp>
2018-05-23 17:30:36 +0900

    modify crawler : Q3 最終ページを取得するよう修正

--- src/jp/co/goalist/crawler/Question3.java
+++ src/jp/co/goalist/crawler/Question3.java
@@ -19,7 +19,14 @@ public class Question3 {
         System.out.println("クロール＆スクレイプ処理 を終了しました。");
     }
 
-    private static void makeArticlesCsv(int year, int month) {
+    private static void makeArticlesCsv(int year, int mon) {
+        String month = null;
+        if(mon < 10) {
+            month = "0" + String.valueOf(mon);
+        }else {
+            month =String.valueOf(mon);
+        }
+
         Path answer = Paths.get("C:/TechTraining/resources/answer.csv");
         try (BufferedWriter bw = Files.newBufferedWriter(answer)) {
             bw.write("件名,カテゴリ,ニュース日時,URL");
@@ -28,9 +35,18 @@ public class Question3 {
             String rootUrl = "http://news.mynavi.jp";
             String subUrl = rootUrl + "/list/headline/" + year + "/" + month; // 指定した年月のページ
 
-            int i = 1;
+            // 最後のページを調べます
+            String lastPageNum = null;
+            Document docTop = Jsoup.connect(subUrl).get();
+            try {
+                Element elPageNum = docTop.select("a.paginate__last").get(0);
+                String lastPageLink = elPageNum.attr("href");
+                lastPageNum = lastPageLink.substring(lastPageLink.indexOf("?page=") + 6);
+            } catch (IndexOutOfBoundsException e) {
+                lastPageNum = "1";
+            }
 
-            while(i < 300) {
+            for (int i = 1; i < Integer.parseInt(lastPageNum) +1; i++) {
                 try {
                     String headlineUrl = subUrl  + "/?page=" + i; // 何ページ目かを指定
                     Document doc = Jsoup.connect(headlineUrl).get();
@@ -63,13 +79,11 @@ public class Question3 {
                         System.out.println(str);
                         bw.write(str);
                         bw.newLine();
-                    }
-                    try {
+                    } try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                    }
-                    i++;
+                    };
                 } catch(IndexOutOfBoundsException e) {
                     break;
                 }
