657f81bd3fa0e997f0898e6a4417d40295440ed0
HITYORI <m.iwasaki@goalist.co.jp>
2018-05-24 19:38:34 +0900

    クロール課題2

--- src/jp/co/goalist/crawler/Kadai2.java
+++ src/jp/co/goalist/crawler/Kadai2.java
@@ -1,6 +1,10 @@
 package jp.co.goalist.crawler;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,26 +12,31 @@ import org.jsoup.nodes.Element;
 
 public class Kadai2 {
     public static void main(String[] args) throws IOException {
-        String rootUrl= "https://news.mynavi.jp/list/headline/ ";
-        Document doc =Jsoup.connect(rootUrl).get();
-        Element el = doc.select(" div.thumb-s").get(0);//dv.thumbsの0番目の要素？
-        //dv.thumbsは一つしかないだけであって要素番号はある。
-        String year =el.select("p").get(0).text();
-        String newYear=year.substring(0,4);
-        
-        for(Element child : el.children()) {
-            if(child.children().size()<1) {
-                continue;
-            }
-            String title= child.select("a").get(0).text();
-            String genre=child.select("a").get(1).text();
-            String date=child.select("span").get(0).text();
-            String url=child.child(0).child(1).child(0).attr("href");
+        Path filePath =Paths.get("C:\\TechTraining\\resources\\Kadai2.csv");
+        Files.deleteIfExists(filePath);
+        Files.createFile(filePath);
+        try(BufferedWriter bw =Files.newBufferedWriter(filePath)){
+            String rootUrl= "https://news.mynavi.jp/list/headline/ ";
+            Document doc =Jsoup.connect(rootUrl).get();
+            Element el = doc.select(" div.thumb-s").get(0);//dv.thumbsの0番目の要素？
+            //dv.thumbsは一つしかないだけであって要素番号はある。
+            String year =el.select("p").get(0).text();
+            String newYear=year.substring(0,4);
             
-            url="https://news.mynavi.jp"+url;
-            System.out.println(title+","+genre+","+newYear+"/"+date+","+url);
-        }
-        
+            for(Element child : el.children()) {
+                if(child.children().size()<1) {
+                    continue;
+                }
+                String title= child.select("a").get(0).text();
+                String genre=child.select("a").get(1).text();
+                String date=child.select("span").get(0).text();
+                String url=child.child(0).child(1).child(0).attr("href");
+            
+                url="https://news.mynavi.jp"+url;
+                bw.write(title+","+genre+","+newYear+"/"+date+","+url);
+                bw.newLine();
+             }
+         }
     }
 }
 
